a0d463ed952defa84d64bbcb70a38b5fc147c1af
FortuneAghedo04 <aghedofortune@gmail.com>
2023-06-14 16:12:53 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:hello_world/register/sign_in.dart';
-import 'package:hello_world/register/sign_up.dart';
-import'package:hello_world/pages/main_food.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hello_world/register/main_page.dart';
-import 'package:hello_world/splash_screen.dart';
-
-
 
 // TO BE ABLE TO RUN FIREBASE IN YOUR PROJECT
 Future main()  async{
@@ -20,7 +13,3 @@ Future main()  async{
 
   ));
 }
-
-
-
-
